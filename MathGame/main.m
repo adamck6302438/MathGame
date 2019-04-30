d8e949bf39dc596ee9482784cea18d9aa119c734
Ray Kang <adamck6302438@gmail.com>
2019-04-30 12:08:48 -0400

    Initial Commit

---
+++ MathGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MathGame
+//
+//  Created by Frances ZiyiFan on 4/30/19.
+//  Copyright © 2019 Ray Kang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
